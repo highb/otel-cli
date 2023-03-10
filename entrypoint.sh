131d057eb12d5e3792b631482b880ce9c698d0a5
Brandon High <highb@users.noreply.github.com>
2023-03-09 16:04:35 -0800

    remove nounset

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,6 @@
 # Fail on non-zero exit code
 set -e
 
-# Fail on unset variables
-set -o nounset
-
-# `$#` expands to the number of arguments and `$@` expands to the supplied `args`
-printf '%d args:' "$#"
-printf " '%s'" "$@"
-printf '\n'
-
 if [ "${SERVER}" = "true" ]; then
   echo "Starting otel-cli in server mode"
   /otel-cli server tui
